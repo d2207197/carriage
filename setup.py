2056fb842b0719491f7a8d47e5b7866a06852ae5
顏孜羲 <joseph.yen@gmail.com>
2018-03-23 23:10:49 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(name='maybee',
+      version='0.1',
+      description='A Maybe/Optional type for python',
+      author='Yen, Tzu-Hsi',
+      author_email='joseph.yen@gmail.com',
+      url='https://github.com/d2207197/maybee',
+      packages=find_packages(),
+     )
