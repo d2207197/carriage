dc7bf0b8c994991c6d75d3c47ef1f3565862913a
顏孜羲 <joseph.yen@gmail.com>
2018-03-25 00:37:33 +0800

    comparator raise TypeError for non-Maybe Type
    cc:
    Asana:
    by: 顏孜羲 <joseph.yen@gmail.com>

--- tests/test_maybee.py
+++ tests/test_maybee.py
@@ -1,20 +1,52 @@
-import maybee
-from maybee import Just, Nothing
+import pytest
+
+from maybee import Just, Maybe, Nothing, NothingError, maybe
 
 
 def test_none():
-    assert maybee.none(None) is Nothing
+    m = maybe(None)
+    assert m is Nothing
+    with pytest.raises(NothingError):
+        m.get()
+
+    assert m.get_or(0) == 0
+    assert m.get_or(None) is None
+    assert m.get_or(False) is False
+    assert m.get_or(True) is True
+    assert m.get_or("A string") == "A string"
 
 
-def test_false_value():
-    m = maybee.none(False)
+def test_non_none():
+    check_just_value(maybe(30), 30)
+    check_just_value(maybe(False), False)
+    check_just_value(maybe("Hello"), "Hello")
+    d = {'john': 50, 'doe': 10}
+    check_just_value(maybe(d), d)
+
+
+def check_just_value(just, value):
+    assert isinstance(just, Just)
+    assert just.get() == value
+    assert just.get_or(None) == value
+    assert just.get_or("Yo") == value
+
+
+def test_get_attr():
+    class Clz:
+        def __init__(self, data):
+            self.data = data
+
+        def get_data(self):
+            return self.data
+    assert maybe(Clz(30)).get_data().get() == 30
+    assert maybe(Clz(30)).get_data().v == 30
+    maybe(Clz(30)).get_data() == 30
+
+
+def test_function():
+    value = 50
+    m = Maybe.except_call(lambda: value)
     assert isinstance(m, Just)
-    assert m.value is False
-    assert m.get_or(True) is False
-
-
-def test_int_value():
-    m = maybee.none(30)
-    assert isinstance(m, Just)
-    assert m.value == 30
-    assert m.get_or(0) == 30
+    assert m.get() == value
+    assert m.get_or(None) == value
+    assert m.get_or("Yo") == value
