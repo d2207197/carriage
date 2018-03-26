24bea1862f0156bfa82964548a6eb7d15f8c898b
顏孜羲 <joseph.yen@gmail.com>
2018-03-27 01:31:26 +0800

    change Maybe to Optional
    cc:
    Asana:
    by: 顏孜羲 <joseph.yen@gmail.com>

--- maybee/maybe.py
+++ maybee/maybe.py
@@ -1,34 +1,79 @@
-
 from abc import ABC, abstractmethod, abstractproperty
+from functools import wraps
 
 
 class NothingError(AttributeError):
     pass
 
 
-class Maybe(ABC):
+class Optional(ABC):
 
     @classmethod
-    def may_except(cls, getter, errors=Exception):
+    def call_exceptable(cls, getter, *args, errors=Exception, **kwargs):
         try:
-            value = getter()
+            value = getter(*args, **kwargs)
         except errors:
             return Nothing_inst
         else:
-            return Just(value)
+            return Some(value)
+
+    ecall = call_exceptable
 
     @classmethod
-    def may_none(self, value):
+    def value_noneable(cls, value):
         if value is None:
             return Nothing_inst
-        return Just(value)
+        return Some(value)
+
+    nval = value_noneable
+
+    @classmethod
+    def noneable(cls, f):
+
+        if not callable(f):
+            raise TypeError("'f' should be a callable: {f!r}")
+
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            res = f(*args, **kwargs)
+            if res is None:
+                return Nothing_inst
+            return Some(res)
+
+        return wrapper
+
+    @classmethod
+    def exceptable(cls, f_or_errors):
+        if callable(f_or_errors):
+            f = f_or_errors
+            errors = Exception
+            return cls._wrap_exceptable(f, errors)
+        else:
+            errors = f_or_errors
+
+            def deco(f):
+                return cls._wrap_exceptable(f, errors)
+            return deco
+
+    @classmethod
+    def _wrap_exceptable(cls, f, errors):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            try:
+                res = f(*args, **kwargs)
+            except errors:
+                return Nothing_inst
+            else:
+                return Some(res)
+
+        return wrapper
 
     @abstractmethod
     def bind(self, maybe_action):
         raise NotImplementedError()
 
     @abstractmethod
-    def fmap(self, action):
+    def map(self, action):
         raise NotImplementedError()
 
     @abstractmethod
@@ -56,6 +101,10 @@ class Maybe(ABC):
         raise NotImplementedError()
 
     @abstractmethod
+    def get_or_none(self):
+        raise NotImplementedError()
+
+    @abstractmethod
     def is_just(self):
         raise NotImplementedError()
 
@@ -80,59 +129,55 @@ class Maybe(ABC):
         raise NotImplementedError()
 
     def __eq__(self, other):
-        if isinstance(other, Maybe):
+        if isinstance(other, Optional):
             return self._value_for_cmp == other._value_for_cmp
 
         raise TypeError(
             "'==' not supported between instances of "
-            f"'Maybe' and {type(other).__name__!r}")
+            f"'Optional' and {type(other).__name__!r}")
 
     def __ne__(self, other):
-        if isinstance(other, Maybe):
+        if isinstance(other, Optional):
             return self._value_for_cmp != other._value_for_cmp
 
         raise TypeError(
             "'!=' not supported between instances of "
-            f"'Maybe' and {type(other).__name__!r}")
+            f"'Optional' and {type(other).__name__!r}")
 
     def __gt__(self, other):
-        if isinstance(other, Maybe):
+        if isinstance(other, Optional):
             return self._value_for_cmp > other._value_for_cmp
 
         raise TypeError(
             "'>' not supported between instances of "
-            f"'Maybe' and {type(other).__name__!r}")
+            f"'Optional' and {type(other).__name__!r}")
 
     def __lt__(self, other):
-        if isinstance(other, Maybe):
+        if isinstance(other, Optional):
             return self._value_for_cmp < other._value_for_cmp
 
         raise TypeError(
             "'<' not supported between instances of "
-            f"'Maybe' and {type(other).__name__!r}")
+            f"'Optional' and {type(other).__name__!r}")
 
     def __ge__(self, other):
-        if isinstance(other, Maybe):
+        if isinstance(other, Optional):
             return self._value_for_cmp >= other._value_for_cmp
 
         raise TypeError(
             "'>=' not supported between instances of "
-            f"'Maybe' and {type(other).__name__!r}")
+            f"'Optional' and {type(other).__name__!r}")
 
     def __le__(self, other):
-        if isinstance(other, Maybe):
+        if isinstance(other, Optional):
             return self._value_for_cmp <= other._value_for_cmp
 
         raise TypeError(
             "'<=' not supported between instances of "
-            f"'Maybe' and {type(other).__name__!r}")
+            f"'Optional' and {type(other).__name__!r}")
 
 
-maybe_none = Maybe.may_none
-maybe_except = Maybe.may_except
-
-
-class Nothing(Maybe):
+class Nothing(Optional):
     __instance = None
 
     def __new__(cls):
@@ -144,7 +189,7 @@ class Nothing(Maybe):
     def bind(self, maybe_action):
         return Nothing_inst
 
-    def fmap(self, action):
+    def map(self, action):
         return Nothing_inst
 
     def then(self, maybe_value):
@@ -164,6 +209,9 @@ class Nothing(Maybe):
 
     def get_or(self, else_value=None):
         return else_value
+
+    def get_or_none(self):
+        return None
 
     def is_just(self):
         return False
@@ -190,29 +238,29 @@ Nothing_inst = Nothing()
 def identity(_): return _
 
 
-class Just(Maybe):
+class Some(Optional):
 
     def __init__(self, value):
         self._just_value = value
 
     def bind(self, maybe_action):
         value = maybe_action(self._just_value)
-        if isinstance(value, Maybe):
+        if isinstance(value, Optional):
             return value
         else:
-            raise TypeError('function should return a Maybe')
+            raise TypeError('function should return a Optional')
 
-    def fmap(self, action):
-        return Just(action(self._just_value))
+    def map(self, action):
+        return Some(action(self._just_value))
 
     def then(self, maybe_value):
         return maybe_value
 
     def join(self):
-        if isinstance(self._just_value, Maybe):
+        if isinstance(self._just_value, Optional):
             return self._just_value
         else:
-            raise TypeError('value should be a Maybe')
+            raise TypeError('value should be a Optional')
 
     def ap(self, maybe_value):
         if maybe_value.is_just():
@@ -231,6 +279,9 @@ class Just(Maybe):
     def get_or(self, else_value=None):
         return self._just_value
 
+    def get_or_none(self):
+        return self._just_value
+
     def is_just(self):
         return True
 
@@ -244,20 +295,20 @@ class Just(Maybe):
         yield self._just_value
 
     def __repr__(self):
-        return f'Just({self._just_value!r})'
+        return f'Some({self._just_value!r})'
 
     def __str__(self):
-        return f'Just({self._just_value!s})'
+        return f'Some({self._just_value!s})'
 
 
-class JustOp(Just):
+class SomeOp(Some):
 
     def __call__(self, *args, **kwargs):
-        return Just(self._just_value(*args, **kwargs))
+        return Some(self._just_value(*args, **kwargs))
 
     def __getattr__(self, name):
         if hasattr(self._just_value, name):
-            return Just(getattr(self._just_value, name))
+            return Some(getattr(self._just_value, name))
         else:
             return Nothing_inst
 
@@ -268,7 +319,7 @@ class JustOp(Just):
         return setattr(self._just_value, name, v)
 
     def __getitem__(self, key):
-        return Just(self._just_value[key])
+        return Some(self._just_value[key])
 
     def __setitem__(self, key, value):
         self._just_value[key] = value
@@ -283,13 +334,13 @@ class JustOp(Just):
         return iter(self._just_value)
 
     def __reversed__(self):
-        return Just(reversed(self._just_value))
+        return Some(reversed(self._just_value))
 
     # def __missing__(self, key):
         # klass = self._just_value.__class__
         # if hasattr(klass, '__missing__') and \
         #         callable(getattr(klass, '__missing__')):
-        #     return Just(self._just_value.__missing__(key))
+        #     return Some(self._just_value.__missing__(key))
 
         # return Nothing_inst
 
@@ -324,82 +375,82 @@ class JustOp(Just):
         return dir(self._just_value)
 
     def __add__(self, other):
-        return Just(self._just_value + other)
+        return Some(self._just_value + other)
 
     def __sub__(self, other):
-        return Just(self._just_value - other)
+        return Some(self._just_value - other)
 
     def __mul__(self, other):
-        return Just(self._just_value * other)
+        return Some(self._just_value * other)
 
     def __floordiv__(self, other):
-        return Just(self._just_value // other)
+        return Some(self._just_value // other)
 
     def __div__(self, other):
-        return Just(self._just_value / other)
+        return Some(self._just_value / other)
 
     def __mod__(self, other):
-        return Just(self._just_value % other)
+        return Some(self._just_value % other)
 
     def __divmod__(self, other):
-        return Just(divmod(self._just_value, other))
+        return Some(divmod(self._just_value, other))
 
     def __pow__(self, other):
-        return Just(self._just_value ** other)
+        return Some(self._just_value ** other)
 
     def __lshift__(self, other):
-        return Just(self._just_value << other)
+        return Some(self._just_value << other)
 
     def __rshift__(self, other):
-        return Just(self._just_value >> other)
+        return Some(self._just_value >> other)
 
     def __and__(self, other):
-        return Just(self._just_value & other)
+        return Some(self._just_value & other)
 
     def __or__(self, other):
-        return Just(self._just_value | other)
+        return Some(self._just_value | other)
 
     def __xor__(self, other):
-        return Just(self._just_value ^ other)
+        return Some(self._just_value ^ other)
 
     def __radd__(self, other):
-        return Just(other + self._just_value)
+        return Some(other + self._just_value)
 
     def __rsub__(self, other):
-        return Just(other - self._just_value)
+        return Some(other - self._just_value)
 
     def __rmul__(self, other):
-        return Just(other * self._just_value)
+        return Some(other * self._just_value)
 
     def __rfloordiv__(self, other):
-        return Just(other // self._just_value)
+        return Some(other // self._just_value)
 
     def __rdiv__(self, other):
-        return Just(other / self._just_value)
+        return Some(other / self._just_value)
 
     def __rmod__(self, other):
-        return Just(other % self._just_value)
+        return Some(other % self._just_value)
 
     def __rdivmod__(self, other):
-        return Just(divmod(other, self._just_value))
+        return Some(divmod(other, self._just_value))
 
     def __rpow__(self, other):
-        return Just(other ** self._just_value)
+        return Some(other ** self._just_value)
 
     def __rlshift__(self, other):
-        return Just(other << self._just_value)
+        return Some(other << self._just_value)
 
     def __rrshift__(self, other):
-        return Just(other >> self._just_value)
+        return Some(other >> self._just_value)
 
     def __rand__(self, other):
-        return Just(other & self._just_value)
+        return Some(other & self._just_value)
 
     def __ror__(self, other):
-        return Just(other | self._just_value)
+        return Some(other | self._just_value)
 
     def __rxor__(self, other):
-        return Just(other ^ self._just_value)
+        return Some(other ^ self._just_value)
 
     def __iadd__(self, other):
         self._just_value += other
@@ -450,108 +501,108 @@ class JustOp(Just):
         return self
 
 
-class JustMaybeNone(Just):
+class SomeOptionalNone(Some):
     def __init__(self, value):
         self._just_value = value
 
     def __call__(self, *args, **kwargs):
-        return Maybe(self._just_value(*args, **kwargs))
+        return Optional(self._just_value(*args, **kwargs))
 
     def __getattr__(self, name):
         attr = getattr(self._just_value, name)
         if callable(attr):
-            return JustMaybeNone(attr)
-        return Maybe(attr)
+            return SomeOptionalNone(attr)
+        return Optional(attr)
 
     def __getitem__(self, key):
-        return Maybe(self._just_value[key])
+        return Optional(self._just_value[key])
 
     def __reversed__(self):
-        return Maybe(reversed(self._just_value))
+        return Optional(reversed(self._just_value))
 
     def __missing__(self, key):
         # TODO: review
         klass = self._just_value.__class__
         if (hasattr(klass, '__missing__') and
                 callable(getattr(klass, '__missing__'))):
-            return Maybe(self._just_value.__missing__(key))
+            return Optional(self._just_value.__missing__(key))
 
         return Nothing_inst
 
     def __add__(self, other):
-        return Maybe(self._just_value + other)
+        return Optional(self._just_value + other)
 
     def __sub__(self, other):
-        return Maybe(self._just_value - other)
+        return Optional(self._just_value - other)
 
     def __mul__(self, other):
-        return Maybe(self._just_value * other)
+        return Optional(self._just_value * other)
 
     def __floordiv__(self, other):
-        return Maybe(self._just_value // other)
+        return Optional(self._just_value // other)
 
     def __div__(self, other):
-        return Maybe(self._just_value / other)
+        return Optional(self._just_value / other)
 
     def __mod__(self, other):
-        return Maybe(self._just_value % other)
+        return Optional(self._just_value % other)
 
     def __divmod__(self, other):
-        return Maybe(divmod(self._just_value, other))
+        return Optional(divmod(self._just_value, other))
 
     def __pow__(self, other):
-        return Maybe(self._just_value ** other)
+        return Optional(self._just_value ** other)
 
     def __lshift__(self, other):
-        return Maybe(self._just_value << other)
+        return Optional(self._just_value << other)
 
     def __rshift__(self, other):
-        return Maybe(self._just_value >> other)
+        return Optional(self._just_value >> other)
 
     def __and__(self, other):
-        return Maybe(self._just_value & other)
+        return Optional(self._just_value & other)
 
     def __or__(self, other):
-        return Maybe(self._just_value | other)
+        return Optional(self._just_value | other)
 
     def __xor__(self, other):
-        return Maybe(self._just_value ^ other)
+        return Optional(self._just_value ^ other)
 
     def __radd__(self, other):
-        return Maybe(other + self._just_value)
+        return Optional(other + self._just_value)
 
     def __rsub__(self, other):
-        return Maybe(other - self._just_value)
+        return Optional(other - self._just_value)
 
     def __rmul__(self, other):
-        return Maybe(other * self._just_value)
+        return Optional(other * self._just_value)
 
     def __rfloordiv__(self, other):
-        return Maybe(other // self._just_value)
+        return Optional(other // self._just_value)
 
     def __rdiv__(self, other):
-        return Maybe(other / self._just_value)
+        return Optional(other / self._just_value)
 
     def __rmod__(self, other):
-        return Maybe(other % self._just_value)
+        return Optional(other % self._just_value)
 
     def __rdivmod__(self, other):
-        return Maybe(divmod(other, self._just_value))
+        return Optional(divmod(other, self._just_value))
 
     def __rpow__(self, other):
-        return Maybe(other ** self._just_value)
+        return Optional(other ** self._just_value)
 
     def __rlshift__(self, other):
-        return Maybe(other << self._just_value)
+        return Optional(other << self._just_value)
 
     def __rrshift__(self, other):
-        return Maybe(other >> self._just_value)
+        return Optional(other >> self._just_value)
 
     def __rand__(self, other):
-        return Maybe(other & self._just_value)
+        return Optional(other & self._just_value)
 
     def __ror__(self, other):
-        return Maybe(other | self._just_value)
+        return Optional(other | self._just_value)
 
     def __rxor__(self, other):
-        return Maybe(other ^ self._just_value)
+        return Optional(other ^ self._just_value)
--- tests/test_maybe.py
+++ tests/test_maybe.py
@@ -1,52 +1,137 @@
 import pytest
 
-from maybee import Just, Nothing, NothingError, maybe_none
+from maybee import Nothing, NothingError, Optional, Some
 
 
-def test_maybe_none_init():
-    nothing = maybe_none(None)
+def test_value_noneable_init():
+    nothing = Optional.value_noneable(None)
     assert nothing is Nothing
     with pytest.raises(NothingError):
         Nothing.value
 
-    just = maybe_none(30)
-    assert type(just) is Just
-    assert just.value == 30
+    some = Optional.value_noneable(30)
+    assert type(some) is Some
+    assert some.value == 30
 
-    just = maybe_none("hello")
-    assert type(just) is Just
-    assert just.value == "hello"
+    some = Optional.value_noneable("hello")
+    assert type(some) is Some
+    assert some.value == "hello"
 
     adict = {'apple': 1, 'orange': 2}
-    just = maybe_none(adict)
-    assert type(just) is Just
-    assert just.value == {'apple': 1, 'orange': 2}
-    assert just.value is adict
+    some = Optional.value_noneable(adict)
+    assert type(some) is Some
+    assert some.value == {'apple': 1, 'orange': 2}
+    assert some.value is adict
 
 
-def test_just_value():
-    assert Just(30).value == 30
-    assert Just(None).value is None
-    assert Just(None).value is None
-    assert Just([1, 2, 3]).value == [1, 2, 3]
+def test_call_exceptable():
+    def raise_exception():
+        raise Exception
+    assert Optional.call_exceptable(raise_exception) is Nothing
+
+
+def test_some_value():
+    assert Some(30).value == 30
+    assert Some(None).value is None
+    assert Some(None).value is None
+    assert Some([1, 2, 3]).value == [1, 2, 3]
     alist = [4, 5, 6]
-    assert Just(alist).value is alist
-    assert Just({'apple': 1, 'orange': 2}).value == {'apple': 1, 'orange': 2}
+    assert Some(alist).value is alist
+    assert Some({'apple': 1, 'orange': 2}).value == {'apple': 1, 'orange': 2}
     adict = {'apple': 1, 'orange': 2}
-    assert Just(adict).value is adict
+    assert Some(adict).value is adict
 
 
 def test_get_or():
-    assert Just(30).get_or(None) == 30
-    assert Just(None).get_or(None) is None
-    assert Just(None).get_or(True) is None
-    assert Just([1, 2, 3]).get_or(True) == [1, 2, 3]
+    assert Some(30).get_or(None) == 30
+    assert Some(None).get_or(None) is None
+    assert Some(None).get_or(True) is None
+    assert Some([1, 2, 3]).get_or(True) == [1, 2, 3]
 
     assert Nothing.get_or(0) == 0
     assert Nothing.get_or(None) is None
     assert Nothing.get_or(False) is False
     assert Nothing.get_or(True) is True
     assert Nothing.get_or("A string") == "A string"
+
+
+def test_map():
+    def multiply_2(n):
+        return n * 2
+    res = Some(100).map(multiply_2)
+    assert res.value == 200
+
+    res = Nothing.map(multiply_2)
+    assert res is Nothing
+
+    def append_1_no_return(alist):
+        alist.append(1)
+
+    res = Some([1, 2, 3]).map(append_1_no_return)
+    assert res.value is None
+
+    def append_1_return(alist):
+        alist.append(1)
+        return alist
+
+    res = Some([1, 2, 3]).map(append_1_return)
+    assert res.value == [1, 2, 3, 1]
+
+    alist = [1, 2, 3]
+    res = Some(alist).map(append_1_return)
+    assert res.value is alist
+
+    res = Nothing.map(multiply_2)
+    assert res is Nothing
+
+
+def test_bind():
+    def get_foo_option(adict):
+        key = 'foo'
+        if key in adict:
+            return Some(adict[key])
+        return Nothing
+    foo_adict = {'foo': 'value of foo', 'bar': 'value of bar'}
+    nofoo_adict = {'no_foo': 'value of foo', 'bar': 'value of bar'}
+
+    res = Some(foo_adict).bind(get_foo_option)
+    assert res.value == 'value of foo'
+
+    res = Nothing.bind(get_foo_option)
+    assert res is Nothing
+
+    res = Some(nofoo_adict).bind(get_foo_option)
+    assert res is Nothing
+
+
+def test_comparator():
+    some_hello = Some("hello")
+    some_hello_2 = Some("hello")
+    some_world = Some("world")
+
+    assert some_hello == some_hello_2
+    with pytest.raises(TypeError):
+        assert some_hello == "hello"
+
+    assert some_hello != some_world
+    with pytest.raises(TypeError):
+        assert some_hello != "world"
+
+    some_100 = Some(100)
+    some_100_2 = Some(100)
+    some_200 = Some(200)
+    assert some_100 < some_200
+    assert some_200 > some_100
+    assert some_100.value < 200
+    assert some_200.value > 100
+    assert some_100 <= some_200
+    assert some_200 >= some_100
+    assert some_100 <= some_100_2
+    assert some_100 >= some_100_2
+    with pytest.raises(TypeError):
+        assert some_200 < 100
+    with pytest.raises(TypeError):
+        assert some_200 > 100
 
 
 def test_get_attr():
@@ -71,73 +156,9 @@ def test_get_attr():
 
     # assert maybe_obj.get_data().v == 30
     # assert maybe_obj.get_none().v is None
-    # assert maybe_obj.get_none() == Just(None)
+    # assert maybe_obj.get_none() == Some(None)
     # assert maybe_obj.get_none() is not Nothing
     # assert maybe_obj.fmap_one.get_none().v is None
     # assert maybe_obj.n.get_none() is Nothing
     # assert maybe_obj.get_none.n() is Nothing
     # assert maybe_obj.join()
-
-
-def test_dict():
-
-    d = {
-        '1': {
-            '2': {
-                '3': 123
-            },
-            '3': 13,
-        },
-        '2': 2
-    }
-
-
-def test_eq_ne_comparator():
-    just_hello = Just("hello")
-    just_hello_2 = Just("hello")
-    just_world = Just("world")
-
-    assert just_hello == just_hello_2
-    with pytest.raises(TypeError):
-        assert just_hello == "hello"
-
-    assert just_hello != just_world
-    with pytest.raises(TypeError):
-        assert just_hello != "world"
-
-
-def test_gt_lt_ge_le_comparator():
-    just_100 = Just(100)
-    just_100_2 = Just(100)
-    just_200 = Just(200)
-    assert just_100 < just_200
-    assert just_200 > just_100
-    assert just_100.value < 200
-    assert just_200.value > 100
-    assert just_100 <= just_200
-    assert just_200 >= just_100
-    assert just_100 <= just_100_2
-    assert just_100 >= just_100_2
-    with pytest.raises(TypeError):
-        assert just_200 < 100
-    with pytest.raises(TypeError):
-        assert just_200 > 100
-
-
-def test_fmap():
-    def multiply_2(n):
-        return n * 2
-    fmap_res = Just(100).fmap(multiply_2)
-    fmap_res.value == 200
-
-    fmap_res = Nothing.fmap(multiply_2)
-    fmap_res is Nothing
-
-
-# def test_function():
-#     value = 50
-#     m = Maybe.except_call(lambda: value)
-#     assert isinstance(m, Just)
-#     assert m.get() == value
-#     assert m.get_or(None) == value
-#     assert m.get_or("Yo") == value
