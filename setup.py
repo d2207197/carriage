af0818f010b2227b9ad70ee76ea7898cc8ef3100
顏孜羲 <joseph.yen@gmail.com>
2018-03-27 22:49:14 +0800

    version 0.1dev
    cc:
    Asana:
    by: 顏孜羲 <joseph.yen@gmail.com>

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from setuptools import find_packages, setup
 
 setup(name='carriage',
-      version='0.1',
+      version='0.1dev',
       description='Enhanced collection classes for programming fluently',
       author='Yen, Tzu-Hsi',
       author_email='joseph.yen@gmail.com',
       url='https://github.com/d2207197/carriage',
       packages=find_packages(),
-     )
+      )
