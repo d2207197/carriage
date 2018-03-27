3335a32c8038d85ce8e2dff6f300200b42ccc325
顏孜羲 <joseph.yen@gmail.com>
2018-03-27 23:21:17 +0800

    add classifiers to setup.py
    cc:
    Asana:
    by: 顏孜羲 <joseph.yen@gmail.com>

--- setup.py
+++ setup.py
@@ -9,4 +9,24 @@ setup(name='carriage',
       author_email='joseph.yen@gmail.com',
       url='https://github.com/d2207197/carriage',
       packages=find_packages(),
+      classifiers=[
+          # How mature is this project? Common values are
+          #   3 - Alpha
+          #   4 - Beta
+          #   5 - Production/Stable
+          'Development Status :: 3 - Alpha',
+
+          # Indicate who your project is intended for
+          'Intended Audience :: Developers',
+          'Software Development :: Libraries :: Python Modules'
+
+
+          # Pick your license as you wish (should match "license" above)
+          'License :: OSI Approved :: MIT License',
+
+          # Specify the Python versions you support here. In particular, ensure
+          # that you indicate whether you support Python 2, Python 3 or both.
+          'Programming Language :: Python :: 3.6',
+      ],
+
       )
