9582b05015a4e678dba383d7fc2f8deebf7e74d6
顏孜羲 <joseph.yen@gmail.com>
2018-03-29 01:59:20 +0800

    add a lot List transform methods
    cc:
    Asana:
    by: 顏孜羲 <joseph.yen@gmail.com>

--- carriage/sequence.py
+++ carriage/sequence.py
@@ -1,8 +1,11 @@
 
 import collections
 import itertools as itt
+from copy import copy
 
 from .monad import Monad
+from .optional import Nothing_inst as Nothing
+from .optional import Optional, Some
 
 
 class List(Monad):
@@ -37,12 +40,14 @@ class List(Monad):
         return List(itt.chain.from_iterable(self._items))
 
     def then(self, alist):
+        # TODO
         if len(self._items) > 0:
             return alist
         else:
             return self
 
     def ap(self, avalue):
+        # TODO
         pass
 
     def __len__(self):
@@ -57,6 +62,89 @@ class List(Monad):
     @property
     def _value_for_cmp(self):
         return self._items
+
+    def len(self):
+        return len(self)
+
+    def take(self, n):
+        return List(self._items[:n])
+
+    def drop(self, n):
+        return List(self._items[n:])
+
+    def slice(self, start, stop, step=None):
+        return self[slice(start, stop, step)]
+
+    def first(self):
+        return self._items[0]
+
+    def last(self):
+        return self._items[-1]
+
+    def filter(self, pred):
+        return List(filter(pred, self._items))
+
+    def first_optional(self):
+        return self.get_optional(0)
+
+    def last_optional(self):
+        return self.get_optional(-1)
+
+    def get_optional(self, idx):
+        if idx < len(self):
+            return Some(self._items[idx])
+
+        return Nothing
+
+    def get(self, idx, default):
+        self._items.get(idx, default)
+
+    def copy(self):
+        return List(copy(self._items))
+
+    def extend(self, iterable):
+        self._items.extend(iterable)
+        return self
+
+    def append(self, item):
+        self._items.append(item)
+        return self
+
+    def appended(self, item):
+        alist = self.copy()
+        alist.append(item)
+        return alist
+
+    def reversed(self):
+        return List(reversed(self._items))
+
+    def distincted(self):
+
+        def distinct(items):
+            item_set = set()
+            for item in items:
+                if item not in item_set:
+                    item_set.add(item)
+                    yield item
+
+        return List(distinct(self._items))
+
+    def sorted(self, key=None, reverse=False):
+        return List(sorted(self._items, key=key, reverse=reverse))
+
+    def take_while(self, pred):
+        def take_while(items):
+            for item in items:
+                if not pred(item):
+                    break
+                yield item
+
+        return List(take_while(self._items))
+
+    def __getitem__(self, idx):
+        if isinstance(idx, slice):
+            return List(self._item[idx])
+        return self._items[idx]
 
 
 class Stream(Monad):
