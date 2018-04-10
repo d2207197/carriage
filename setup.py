712278c974330092aa1c4194f977f8dee576135a
顏孜羲 <joseph.yen@gmail.com>
2018-04-10 11:57:10 +0800

    version 0.2.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ extras_require = {'pandas': ['pandas']}
 extras_require['all'] = set(itt.chain.from_iterable(extras_require.values()))
 
 setup(name='carriage',
-      version='0.2',
+      version='0.2.1',
       description='Enhanced collection classes for programming fluently',
       author='Yen, Tzu-Hsi',
       author_email='joseph.yen@gmail.com',
